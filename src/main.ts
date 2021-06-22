3d291818b30a098cb6f5b042db7d69642fa11ec4
Kerry Shetline <kerry@shetline.com>
2021-06-22 01:01:24 -0400

    Adopt to new @tubular hybrid modules.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { initTimezoneLargeAlt, pollForTimezoneUpdates, zonePollerBrowser } from '@tubular/time';
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-initTimezoneLargeAlt(true);
-pollForTimezoneUpdates(zonePollerBrowser, 'large-alt');
+import * as tbABR from '@tubular/array-buffer-reader';
+import * as tbMath from '@tubular/math';
+import * as tbTime from '@tubular/time';
+import * as tbUtil from '@tubular/util';
 
-if (environment.production)
-  enableProdMode();
+(window as any)['@tubular/array-buffer-reader'] = tbABR;
+(window as any)['@tubular/math'] = tbMath;
+(window as any)['@tubular/time'] = tbTime;
+(window as any)['@tubular/util'] = tbUtil;
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+import('./app/app.module').then(pkg => {
+  tbTime.initTimezoneLargeAlt(true);
+  tbTime.pollForTimezoneUpdates(tbTime.zonePollerBrowser, 'large-alt');
+
+  if (environment.production)
+    enableProdMode();
+
+  platformBrowserDynamic().bootstrapModule(pkg.AppModule)
+    .catch(err => console.log(err));
+});
