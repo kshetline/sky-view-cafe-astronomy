4a87fdc04af138656805421e68bd910b84ac210d
Kerry Shetline <kerry@shetline.com>
2021-06-23 01:17:42 -0400

    Update to @tubular packages with improved fesm2015 bundles. Adjust .browserslistrc to prevent generation of ES5 code.

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,15 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { initTimezoneLargeAlt, pollForTimezoneUpdates, zonePollerBrowser } from '@tubular/time';
+import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import * as tbABR from '@tubular/array-buffer-reader';
-import * as tbMath from '@tubular/math';
-import * as tbTime from '@tubular/time';
-import * as tbUtil from '@tubular/util';
+initTimezoneLargeAlt(true);
+pollForTimezoneUpdates(zonePollerBrowser, 'large-alt');
 
-(window as any)['@tubular/array-buffer-reader'] = tbABR;
-(window as any)['@tubular/math'] = tbMath;
-(window as any)['@tubular/time'] = tbTime;
-(window as any)['@tubular/util'] = tbUtil;
+if (environment.production)
+  enableProdMode();
 
-import('./app/app.module').then(pkg => {
-  tbTime.initTimezoneLargeAlt(true);
-  tbTime.pollForTimezoneUpdates(tbTime.zonePollerBrowser, 'large-alt');
-
-  if (environment.production)
-    enableProdMode();
-
-  platformBrowserDynamic().bootstrapModule(pkg.AppModule)
-    .catch(err => console.log(err));
-});
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
