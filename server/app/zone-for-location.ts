b026fceac742ffabc8842b4bc3c851dcc289e1b5
Kerry Shetline <kerry@shetline.com>
2022-07-15 22:27:11 -0400

    Update zoneloc API call to use geo-tz.

--- server/app/zone-for-location.ts
+++ server/app/zone-for-location.ts
@@ -3,8 +3,10 @@ import { processMillis, toBoolean, toNumber } from '@tubular/util';
 import { asyncHandler } from './common';
 import { requestJson } from 'by-request';
 import { pool } from './atlas-database';
+import { code2ToCode3 } from './gazetteer';
 import { PoolConnection } from './mysql-await-async';
 import { Timezone } from '@tubular/time';
+import { find } from 'geo-tz';
 
 export const router = Router();
 
@@ -23,37 +25,38 @@ export async function getTimezoneForLocation(lat: number, lon: number, time = 0)
   if (time === 0)
     time = Math.floor(processMillis() / 1000);
 
+  const zoneByGeoTz = find(lat, lon)[0];
   let connection: PoolConnection;
+  let data: TzInfo;
 
   try {
     connection = await pool.getConnection();
 
     zoneLoop:
     for (const span of [0.05, 0.1, 0.25, 0.5]) {
-      const query = 'SELECT time_zone, country FROM atlas2 WHERE latitude >= ? AND latitude <= ? AND longitude >= ? AND longitude <= ?';
+      const query = 'SELECT timezone, country FROM gazetteer WHERE latitude >= ? AND latitude <= ? AND longitude >= ? AND longitude <= ?';
       const results = (await connection.queryResults(query, [lat - span, lat + span, lon - span, lon + span])) || [];
-      let timeZoneId: string;
+      let timeZoneId = zoneByGeoTz;
       let country: string;
 
       for (const result of results) {
-        if (result.time_zone) {
+        if (!zoneByGeoTz && result.timezone) {
           if (!timeZoneId)
-            timeZoneId = result.time_zone;
-          else if (timeZoneId !== result.time_zone)
-            break zoneLoop;
-
-          if (!country)
-            country = result.country;
-          else if (country !== result.country)
+            timeZoneId = result.timezone;
+          else if (timeZoneId !== result.timezone)
             break zoneLoop;
         }
+
+        if (!country)
+          country = result.country;
+        else if (country !== result.country)
+          break zoneLoop;
       }
 
       if (timeZoneId) {
-        connection.release();
         const zone = Timezone.getTimezone(timeZoneId);
 
-        return {
+        data = {
           timeZoneId,
           country,
           dstOffset: zone.dstOffset,
@@ -61,27 +64,44 @@ export async function getTimezoneForLocation(lat: number, lon: number, time = 0)
           status: 'OK',
           fromDb: true
         };
+
+        break;
       }
     }
   }
-  catch {
-    if (connection) {
-      try {
-        connection.release();
-      }
-      catch {}
-    }
-  }
+  catch {}
+
+  connection?.release();
 
   const key = encodeURIComponent(process.env.GOOGLE_API_KEY);
-  const url = `https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lon}&timestamp=${time}&key=${key}`;
-  let data: TzInfo;
 
-  try {
-    data = await requestJson(url);
+  if (!data) {
+    const url = `https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lon}&timestamp=${time}&key=${key}`;
+
+    try {
+      data = await requestJson(url);
+    }
+    catch (err) {
+      data = { status: 'ERROR', errorMessage: err.toString() };
+    }
   }
-  catch (err) {
-    data = { status: 'ERROR', errorMessage: err.toString() };
+
+  if (data && !data.country) {
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?key=${key}&result_type=locality|administrative_area_level_3&latlng=${lat},${lon}`;
+
+    try {
+      const addr = ((await requestJson(url))?.results || [])[0]?.address_components;
+
+      if (addr) {
+        const country = addr?.find((comp: any) => comp.types?.includes('country'))?.short_name;
+
+        if (country)
+          data.country = code2ToCode3[country] || country;
+      }
+    }
+    catch (err) {
+      data = { status: 'ERROR', errorMessage: err.toString() };
+    }
   }
 
   return data;
